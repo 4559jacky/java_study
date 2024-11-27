0076a06e2616fec2e8aea873ab9ef5a64254038f
4559jacky <4559jacky@gmail.com>
2024-11-27 11:10:56 +0900

    [jji] Practice03 커밋
    	- charAt 배워보기

---
+++ lec02_variable/src/com/gn/practice/Practice03.java
@@ -0,0 +1,11 @@
+package com.gn.practice;
+
+public class Practice03 {
+
+	public static void main(String[] args) {
+		String full_name = "제갈민";
+		char last_name = full_name.charAt(2);
+		System.out.println(last_name + "이에요!!");
+	}
+
+}
