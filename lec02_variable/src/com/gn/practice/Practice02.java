ca922dca256462fc2482419d3ed960d67a16d89d
4559jacky <4559jacky@gmail.com>
2024-11-27 10:21:09 +0900

    [jji] 철수의 일기
    	- Practice02 커밋

---
+++ lec02_variable/src/com/gn/practice/Practice02.java
@@ -0,0 +1,24 @@
+package com.gn.practice;
+
+public class Practice02 {
+	public static void main(String[] args) {
+		boolean isStudent = true;
+		System.out.println(isStudent);
+		char grade = 'A';
+		System.out.println(grade);
+		byte candy = 2;
+		System.out.println(candy);
+		short walk = 30000;
+		System.out.println(walk);
+		int day = 45;
+		System.out.println(day);
+		long future = 45L;
+		System.out.println(future);
+		float height = 179.5f;
+		System.out.println(height);
+		double score = 4.3;
+		System.out.println(score);
+		
+		System.out.println(isStudent);
+	}
+}
