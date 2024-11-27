448b8cb64867c5538be6a3d128a56397d3bfc3ef
4559jacky <4559jacky@gmail.com>
2024-11-27 12:13:46 +0900

    [jji] Practice04 커밋
    	- 형변환 공부

---
+++ lec02_variable/src/com/gn/practice/Practice04.java
@@ -0,0 +1,14 @@
+package com.gn.practice;
+
+public class Practice04 {
+
+	public static void main(String[] args) {
+		int num1 = 10;
+		double num2 = 5.7;
+		int sum1 = num1 + (int)num2;
+		double sum2 = num1 + num2;
+		System.out.println("정수 : " + sum1);
+		System.out.println("실수 : " + sum2);
+	}
+
+}
