1a5069efda0aedbb1d25c149f3993811f8d3528f
4559jacky <4559jacky@gmail.com>
2024-11-28 18:58:07 +0900

    [jji] 과제 재풀이
    	- 더 간결하게 고치고 commit

--- lec03_operator/src/com/gn/homework/Homework02.java
+++ lec03_operator/src/com/gn/homework/Homework02.java
@@ -7,7 +7,7 @@ public class Homework02 {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("구슬의 개수 : " );
 		int num = sc.nextInt();
-		String str = (num%2==0)? "짝수" : "홀수";
-		System.out.println("구슬의 개수는 " + str + "입니다.");
+		String str = (num%2==0)? "구슬의 개수는 짝수입니다." : "구슬의 개수는 홀수입니다.";
+		System.out.println(str);
 	}
 }
