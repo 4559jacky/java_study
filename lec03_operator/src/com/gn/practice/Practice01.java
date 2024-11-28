2e4b683639fe89ec40130f933cb4fadb4c6d5681
4559jacky <4559jacky@gmail.com>
2024-11-28 10:11:33 +0900

    [jji] 비교연산자 commit
    	- 비교연산자 배움

--- lec03_operator/src/com/gn/practice/Practice01.java
+++ lec03_operator/src/com/gn/practice/Practice01.java
@@ -6,18 +6,14 @@ public class Practice01 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("첫번째 숫자를 입력해주세요 : ");
+		System.out.print("첫번째 숫자 : ");
 		int num1 = sc.nextInt();
 		
 		sc.nextLine();
 		
-		System.out.print("두번째 숫자를 입력해주세요 : ");
+		System.out.print("두번째 숫자 : ");
 		int num2 = sc.nextInt();
 		
-		System.out.println();
-		
-		System.out.println("첫번째 숫자 : **" + num1 + "**");
-		System.out.println("두번째 숫자 : **" + num2 + "**");
 		System.out.println("덧셈 : " + (num1+num2));
 		System.out.println("뺄셈 : " + (num1-num2));
 		System.out.println("곱셈 : " + (num1*num2));
---
+++ lec03_operator/src/com/gn/practice/Practice03.java
@@ -0,0 +1,17 @@
+package com.gn.practice;
+
+import java.util.*;
+
+public class Practice03 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("숫자(1) : ");
+		int a = sc.nextInt();
+		
+		System.out.print("숫자(2) : ");
+		int b = sc.nextInt();
+		
+		boolean result = a > b;
+		System.out.println("첫번째 숫자가 두번째 숫자보다 큰가요? " + result);
+	}
+}
