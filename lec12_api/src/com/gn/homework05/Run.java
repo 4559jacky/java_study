0205f1b868b8cf40f0dea10f90600a080d7f7014
4559jacky <4559jacky@gmail.com>
2024-12-16 18:36:10 +0900

    [jji] 과제 5번 수정

--- lec12_api/src/com/gn/homework05/Run.java
+++ lec12_api/src/com/gn/homework05/Run.java
@@ -11,8 +11,8 @@ public class Run {
 		System.out.print("생년월일 : ");
 		String birth = sc.next();
 		String firstName = name.substring(0,1);
-		String year = birth.substring(0,4);
-		String month = birth.substring(4,6);
+//		String year = birth.substring(0,4);
+//		String month = birth.substring(4,6);
 		String password = firstName+birth.substring(2,4)+birth.substring(6,8)+name.substring(1);
 		System.out.println("암호 : "+password);
 	}
